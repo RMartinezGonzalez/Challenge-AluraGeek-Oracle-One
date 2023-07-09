df7a3c8a95b31dc08eb92ce0dd82e900bf6ed61e
R <ric.martinez.gonzalez@gmail.com>
2023-07-09 01:09:29 -0600

    Estilos de contacto

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,23 @@ module.exports = {
       transparent: 'transparent',
       'clr-error': 'hsl(337, 42%, 54%)',
     },
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1089px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
+
   plugins: [],
 };
